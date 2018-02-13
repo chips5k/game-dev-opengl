bf7dd1ae616b500f332d56001a0218ccaac31216
Callem Pittard <callem.pittard@gmail.com>
2018-02-13 22:53:20 +1100

    WIP Shader/triangle tutorial

--- main.cpp
+++ main.cpp
@@ -5,6 +5,15 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
+
+const char* vertexShaderSource = ""
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+        " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\n\0";
+
 int main()
 {
 
@@ -40,7 +49,43 @@ int main()
     //Configure on window resize callback to update the gl viewport
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    float vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f, 0.5f, 0.0f
+    };
+
+    //Setup a variable to track our vertex buffer object id
+    unsigned int VBO;
+    //generate the buffer object and assign its id to our reference
+    glGenBuffers(1, &VBO);
+
+    //Bind the buffer to the array buffer target
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    //Once this has been done, any calls made on GL_ARRAY_BUFFER will use the bound buffer
+
+    //copy our vertex data into the buffer (note we are rendering a still image,
+    //so we specify render type of static (since it wont change much)
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW)
+
+    //setup our vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    //Load in the vertex shader source code
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     //Display loop, iterate until instructed to close
+
+    /* Check if our shader source compiled correctly
+     * Clean this up
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR - failed to compile vertex shader -> " << infoLog << std::endl;
+        return 1;
+    }
+    */
     while (!glfwWindowShouldClose(window))
     {
 
